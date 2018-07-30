987e49c65a03bcbd90087b98e1ba6ab8e9f3516c
Ghost <mkp0203@live.com>
2018-07-30 11:52:47 -0400

    removed timer.js file

--- assets/js/timer.js
+++
@@ -1,8 +0,0 @@
-// var time = 3;
-// var downloadTimer = setInterval(function() {
-//     time--;
-//     document.getElementById("timer").textContent = time;
-//     if(time <= 0) {
-//         clearInterval(downloadTimer);
-//     }
-// },1000);
